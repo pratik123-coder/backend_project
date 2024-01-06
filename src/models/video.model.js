7520560b70b9c71cfff89c3757a9b004389398d2
pratik-win-PC <pratiktravelogue@gmail.com>
2024-01-06 13:29:27 +0530

    User model and Schema model created, JWT and bcrypt implemented.

---
+++ src/models/video.model.js
@@ -0,0 +1,13 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+
+},
+{
+  timestamps:true
+})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.Model("Video", videoSchema)
