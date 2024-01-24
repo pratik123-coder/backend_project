baf6485d3dfa174e09f82c31c4e91bce54b08cda
pratik-win-PC <pratiktravelogue@gmail.com>
2024-01-24 15:56:48 +0530

    Routes written || Async Handler Error resolved

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt, { compare } from "bcrypt";
 
 const userSchema = new Schema({
   username: {
